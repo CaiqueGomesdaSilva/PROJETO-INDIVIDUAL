411713b208142b776379af8e3e21a48cd94270b4
CaiqueGomesdaSilva <caique.gsilva@sptech.school>
2022-11-28 23:13:03 -0300

    criptografia adicionado na senha

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function entrar(username, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", username, senha)
     var instrucao = `
-        SELECT * FROM Usuario WHERE username = '${username}' AND senha = '${senha}';
+        SELECT * FROM Usuario WHERE username = '${username}' AND senha = md5('${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -16,7 +16,7 @@ function cadastrar(username, nome, email, senha, contato) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO Usuario (username, senha, nome, email, telefone) VALUES ('${username}', '${senha}', '${nome}', '${email}', '${contato}');
+        INSERT INTO Usuario (username, senha, nome, email, telefone) VALUES ('${username}', md5('${senha}'), '${nome}', '${email}', '${contato}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
